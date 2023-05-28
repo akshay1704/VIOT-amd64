acf892591e7a8c260ec814deb3e143c4389f0811
Akshay Ajith <akshay@Akshays-MBP-2.lan>
2023-05-28 19:48:20 -0400

    c13

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
 
+image_url="https://drive.google.com/file/d/1CT4z1P6jElWZs5MLh_aOiuZoyPweVMpe/view?usp=share_link"
+FILEID="1CT4z1P6jElWZs5MLh_aOiuZoyPweVMpe"
+FILENAME="/local/repository/qemu-images/image.qcow2"
+
+COMMAND="wget --load-cookies /tmp/cookies.txt \"https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=${FILEID}' -O- | sed -rn 's/.confirm=([0-9A-Za-z_]+)./\1\n/p')&id=${FILEID}\" -O ${FILENAME} && rm -rf /tmp/cookies.txt"
+
+eval "$COMMAND"
+sleep 1
 ############################# 14_recover_public_ip.sh
 
 # echo "Usage: $0 <wan-interface> <guest IP>"
@@ -39,7 +48,7 @@
 
 #nohup python3 run2.py > /dev/null 2>&1 &
 
-python3 run2.py
+#python3 run2.py
 
 sudo qemu-system-x86_64 -machine pc -cpu Nehalem -m 1G \
 -drive file=/local/repository/qemu-images/image.qcow2 -device e1000,netdev=net \
